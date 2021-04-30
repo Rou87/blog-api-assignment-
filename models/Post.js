beb94a69187210088bbd01ecd650081382d6b56e
Raef Sabry <45903766+Rou87@users.noreply.github.com>
2021-04-30 14:13:22 +0200

    Add files via upload

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+const PostSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: Array,
+        required: false
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = mongoose.model('Posts', PostSchema); // <- mongoose.model har skapats och bundits/deklarerad till Post modellen.
